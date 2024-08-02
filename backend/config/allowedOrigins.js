dea95fc0dc1a87461858d0e94842a039acaf2428
Noorhesham <noordragon2004@gmail.com>
2024-08-02 18:27:50 +0200

    semi

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,2 +1,2 @@
-const allowedOrigins = ["http://localhost:3001", "http://localhost:5173"];
+const allowedOrigins = ["http://localhost:3001", "http://localhost:5173", "https://gentle-parfait-8f331e.netlify.app"];
 module.exports = allowedOrigins;
